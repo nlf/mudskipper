5c0f888abf451038f85b2230b92f2a50a4494d55
Nathan LaFreniere <quitlahok@gmail.com>
2013-09-20 13:08:18 -0700

    first pass at supporting plugins

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 test:
-		@node node_modules/lab/bin/lab
+		@node node_modules/lab/bin/lab test/index.js
 test-cov: 
-		@node node_modules/lab/bin/lab -r threshold -t 100
+		@node node_modules/lab/bin/lab -r threshold -t 100 test/index.js
 test-cov-html:
-		@node node_modules/lab/bin/lab -r html -o coverage.html
+		@node node_modules/lab/bin/lab -r html -o coverage.html test/index.js
 complexity:
 		@node node_modules/complexity-report/src/cli.js -o complexity.md -f markdown lib
 
---
+++ test/index.js
@@ -0,0 +1,215 @@
+var Lab = require('lab');
+var Hapi = require('hapi');
+var Types = Hapi.types;
+
+// Declare internals
+var internals = {};
+
+internals.resources = {
+    articles: {
+        index: function (request) {
+            request.reply([]);
+        },
+        show: {
+            handler: function (request) {
+                request.reply([]);
+            },
+            config: {
+                validate: {
+                    path: {
+                        article_id: Types.String()
+                    }
+                }
+            }
+        },
+        create: {
+            handler: function (request) {
+                request.reply('ok');
+            }
+        },
+        test: {
+            path: 'test',
+            handler: function (request) {
+                request.reply('ok');
+            }
+        }
+    },
+    users: {
+        children: ['articles'],
+        index: function (request) {
+            request.reply([]);
+        },
+        show: {
+            handler: function (request) {
+                request.reply('ok');
+            },
+            config: {
+                validate: {
+                    path: {
+                        user_id: Types.String()
+                    }
+                }
+            }
+        },
+        create: {
+            handler: function (request) {
+                request.reply('ok');
+            },
+            config: {
+                payload: 'parse'
+            }
+        }
+    },
+    tests: {
+        index: function (request) {
+            request.reply([]);
+        },
+        children: [
+            {
+                extras: {
+                    index: function (request) {
+                        request.reply([]);
+                    }
+                }
+            }
+        ]
+    },
+    bananas: {
+        basePath: 'banana',
+        index: function (request) {
+            request.reply([]);
+        }
+    }
+};
+
+// Test shortcuts
+var expect = Lab.expect;
+var before = Lab.before;
+var after = Lab.after;
+var describe = Lab.experiment;
+var it = Lab.test;
+
+var server;
+
+describe('mudskipper', function () {
+    it('doesn\'t blow up of you don\'t pass resources on load', function (done) {
+
+        server = new Hapi.Server();
+        server.pack.require('../', null, function (err) {
+
+            expect(err).to.not.exist;
+            done();
+        });
+    });
+
+    it('allows a plugin to add resources via api', function (done) {
+
+        server.pack.register(require('./plugin'), null, function (err) {
+
+            expect(err).to.not.exist;
+            done();
+        });
+    });
+
+    it('can be added as a plugin to hapi', function (done) {
+
+        server = new Hapi.Server();
+        server.pack.require('../', internals.resources, function (err) {
+
+            expect(err).to.not.exist;
+            done();
+        });
+    });
+
+    it('registers routes for articles', function (done) {
+
+        var table = server.routingTable();
+        var found = table.filter(function (route) {
+            return (route.method === 'get' && route.path === '/articles') ||
+                (route.method === 'get' && route.path === '/articles/{article_id}') ||
+                (route.method === 'post' && route.path === '/articles') ||
+                (route.method === 'get' && route.path === '/articles/test');
+        });
+
+        expect(found.length).to.equal(4);
+
+        done();
+    });
+
+    it('registers routes for users', function (done) {
+
+        var table = server.routingTable();
+        var found = table.filter(function (route) {
+            return (route.method === 'get' && route.path === '/users') ||
+                (route.method === 'post' && route.path === '/users');
+        });
+
+        expect(found.length).to.equal(2);
+
+        done();
+    });
+
+    it('registers routes for articles nested on users', function (done) {
+
+        var table = server.routingTable();
+        var found = table.filter(function (route) {
+            return (route.method === 'get' && route.path === '/users/{user_id}/articles') ||
+                (route.method === 'get' && route.path === '/users/{user_id}/articles/{article_id}') ||
+                (route.method === 'post' && route.path === '/users/{user_id}/articles');
+        });
+
+        expect(found.length).to.equal(3);
+
+        done();
+    });
+
+    it('does not lose validation for nested routes', function (done) {
+
+        var table = server.routingTable();
+        var found = table.filter(function (route) {
+            return (route.method === 'get' && route.path === '/users/{user_id}/articles/{article_id}');
+        });
+
+        expect(found[0].settings).to.have.property('validate');
+        expect(found[0].settings.validate).to.have.property('path');
+        expect(found[0].settings.validate.path).to.have.property('article_id');
+        expect(found[0].settings.validate.path).to.have.property('user_id');
+
+        done();
+    });
+
+    it('registers routes for tests', function (done) {
+
+        var table = server.routingTable();
+        var found = table.filter(function (route) {
+            return (route.method === 'get' && route.path === '/tests');
+        });
+
+        expect(found.length).to.equal(1);
+
+        done();
+    });
+
+    it('registers routes for extras nested on tests', function (done) {
+
+        var table = server.routingTable();
+        var found = table.filter(function (route) {
+            return (route.method === 'get' && route.path === '/tests/{test_id}/extras');
+        });
+
+        expect(found.length).to.equal(1);
+
+        done();
+    });
+
+    it('allows you to override the basePath', function (done) {
+        var table = server.routingTable();
+        var found = table.filter(function (route) {
+            return (route.method === 'get' && route.path === '/banana');
+        });
+
+        expect(found.length).to.equal(1);
+
+        done();
+    });
+});
